38ae9b99694b784193c0c978b8b2001990dbf2be
Durai Velu <duraisingam@gmail.com>
2021-12-26 12:04:50 -0500

    Added login API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -4,6 +4,8 @@ from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from profiles_api import serializers
 from profiles_api import models
@@ -106,4 +108,11 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name','email')
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication tokens.
+    Can be added to urls.py but by default it doesnt showup in UI.
+    For testing we add here to understand the auth token workinhg."""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
 #from django.shortcuts import render
